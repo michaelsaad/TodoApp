b3958177eeb76a6a866fd6aa2b329a37c12d4c97
michael saad <michealsaad11@gmail.com>
2024-05-26 01:45:30 +0300

    add todo db

--- todo.sql
+++ todo.sql
@@ -1,15 +1,16 @@
 use todo;
 CREATE TABLE todo (
-    id INT  auto_increment PRIMARY KEY,
-    title VARCHAR(255),
-    user_id INT,
-    todo_details_id INT,
-    FOREIGN KEY (todo_details_ID) REFERENCES todo_details(id)
+                      id INT  auto_increment PRIMARY KEY,
+                      title VARCHAR(255),
+                      user_id INT,
+                      todo_details_id INT,
+                      FOREIGN KEY (todo_details_ID) REFERENCES todo_details(id)
 );
 CREATE TABLE todo_details (
-    id INT auto_increment PRIMARY KEY,
-    description TEXT,
-    created_at TIMESTAMP,
-    priority VARCHAR(50),
-    status VARCHAR(50)
+                              id INT auto_increment PRIMARY KEY,
+                              description TEXT,
+                              priority VARCHAR(50),
+                              status VARCHAR(50),
+                              created_at TIMESTAMP,
+                              last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
