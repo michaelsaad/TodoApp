bb87217e5400b41c2e69c730e211bdaa6120fc19
michael saad <michealsaad11@gmail.com>
2024-05-30 22:32:39 +0300

    comiit

--- todo.sql
+++ todo.sql
@@ -1,33 +1,39 @@
 use todo;
-CREATE TABLE todo (
-    id INT  auto_increment PRIMARY KEY,
-    title VARCHAR(255),
-    user_id INT,
-    todo_details_id INT,
-    FOREIGN KEY (todo_details_ID) REFERENCES todo_details(id)
-);
 CREATE TABLE todo_details (
-    id INT auto_increment PRIMARY KEY,
-    description TEXT,
-    created_at TIMESTAMP,
-    last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    priority ENUM('REAL','CRITICAL','IMPORTANT','ROUTINE') NOT NULL,
-    status ENUM('FINISHED','UNFINISHED','IN_PROGRESS') NOT NULL
+                              id INT auto_increment PRIMARY KEY,
+                              description TEXT,
+                              created_at TIMESTAMP,
+                              last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+                              priority ENUM('REAL','CRITICAL','IMPORTANT','ROUTINE') NOT NULL,
+                              status ENUM('FINISHED','UNFINISHED','IN_PROGRESS') NOT NULL
 );
+CREATE TABLE todo (
+                      id INT  auto_increment PRIMARY KEY,
+                      title VARCHAR(255),
+                      user_id INT,
+                      todo_details_id INT,
+                      FOREIGN KEY (todo_details_ID) REFERENCES todo_details(id)
+);
+
 use user_db ;
 CREATE TABLE user (
-    id INT auto_increment PRIMARY KEY,
-    first_name VARCHAR(50)NOT NULL,
-    last_name VARCHAR(50)NOT NULL,
-    email VARCHAR(100)  NOT NULL UNIQUE,
-    password VARCHAR(200)NOT NULL,
-    otp INT NOT NULL ,
-    enabled BOOLEAN
+                      id INT auto_increment PRIMARY KEY,
+                      first_name VARCHAR(50)NOT NULL,
+                      last_name VARCHAR(50)NOT NULL,
+                      email VARCHAR(100)  NOT NULL UNIQUE,
+                      password VARCHAR(200)NOT NULL,
+                      role ENUM('USER','ADMIN','MANGER') NOT NULL,
+                      verified BOOLEAN DEFAULT false,
+                      otp INT NOT NULL ,
+                      otpGeneratedTime TIMESTAMP NOT NULL
+
 );
 CREATE TABLE  JWT (
-  id INT auto_increment  PRIMARY KEY ,
-  user_id INT  NOT NULL,
-  expiration_date datetime NOT NULL,
-  token_type VARCHAR(50) NOT null ,
-  FOREIGN KEY (user_id) REFERENCES user(id)
+                      id INT auto_increment  PRIMARY KEY ,
+                      token VARCHAR(50)NOT NULL,
+                      revoked BOOLEAN DEFAULT false,
+                      expired BOOLEAN DEFAULT false,
+                      token_type ENUM('BEARER') NOT NULL ,
+                      user_id INT  NOT NULL,
+                      FOREIGN KEY (user_id) REFERENCES user(id)
 );
